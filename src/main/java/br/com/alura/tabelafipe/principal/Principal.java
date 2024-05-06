f07cd14f58f6e4f46c8561bf6f577418abd8000b
Rodrigo Najdek <rodrigo.najdek@gmail.com>
2024-05-06 16:07:26 -0300

    2.1

--- src/main/java/br/com/alura/tabelafipe/principal/Principal.java
+++ src/main/java/br/com/alura/tabelafipe/principal/Principal.java
@@ -1,10 +1,14 @@
 package br.com.alura.tabelafipe.principal;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 import br.com.alura.tabelafipe.model.Dados;
 import br.com.alura.tabelafipe.model.Modelos;
+import br.com.alura.tabelafipe.model.Veiculo;
 import br.com.alura.tabelafipe.service.ConsumoApi;
 import br.com.alura.tabelafipe.service.ConverteDados;
 
@@ -57,6 +61,33 @@ public class Principal {
 			.sorted(Comparator.comparing(Dados::codigo))
 			.forEach(System.out::println);
 		
+		System.out.println("\nDigite um trecho do nome do carro a ser buscado:");
+		var nomeVeiculo = leitura.nextLine();
+		
+		List<Dados> modelosFiltrados = modeloLista.modelos().stream()
+				.filter(m -> m.nome().toLowerCase().contains(nomeVeiculo.toLowerCase()))
+				.collect(Collectors.toList());
+		
+		System.out.println("\nModelos filtrados");
+		modelosFiltrados.forEach(System.out::println);
+		
+		System.out.println("Digite por favor o código do modelo para buscar os valores de avaliação:");
+		var codigoModelo = leitura.nextLine();
+		
+		endereco = endereco + "/" + codigoModelo + "/anos";
+		json = consumo.obterDados(endereco);
+		List<Dados> anos = conversor.obterLista(json, Dados.class);
+		List<Veiculo> veiculos = new ArrayList<>();
+		
+		for (int i = 0; i < anos.size(); i++) {
+			var enderecoAnos = endereco + "/" + anos.get(i).codigo();
+			json = consumo.obterDados(enderecoAnos);
+			Veiculo veiculo = conversor.obterDados(json, Veiculo.class);
+			veiculos.add(veiculo);
+		}
+		
+		System.out.println("\nTodos os veiculos filtrados com avaliações por ano: ");
+		veiculos.forEach(System.out::println);
 	}
 	
 }
